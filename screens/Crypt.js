25326e90ebec942b6372dac48c2e054322fad086
Dacooleric <dacooleric@gmail.com>
2023-09-15 01:37:00 +0000

    added some icons

--- screens/Crypt.js
+++ screens/Crypt.js
@@ -6,41 +6,76 @@ import {
   Platform,
   Dimensions,
   StatusBar,
+  ImageBackground,
+  Image,
 } from "react-native";
 import React from "react";
 
 const screenHeight = Dimensions.get("screen").height;
 
+const backArrow = require("../assets/backarrow.png");
+const bg_image = require("../assets/bg.png");
+const logo = require("../assets/logo.png");
+const favorite = require("../assets/fav.png");
+const email = require("../assets/email.png");
+const socialmedia = require("../assets/social.png");
+const ewallet = require("../assets/ewallet.png");
+const logout = require("../assets/logo.png");
+
 const Crypt = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.categoryContainer}>
+      <ImageBackground
+        source={bg_image}
+        resizeMode="cover"
+        style={styles.categoryContainer}
+      >
         <View style={styles.contentContainer}>
           <View style={styles.backContainer}>
-            <Text>Return</Text>
+            <Image
+              source={backArrow}
+              resizeMode="contain"
+              style={styles.image}
+            />
           </View>
 
           <View style={styles.accountTypeContainer}>
             <View style={styles.favoritesContainer}>
-              <Text>Favorites</Text>
+              <Image
+                source={favorite}
+                resizeMode="contain"
+                style={styles.image}
+              />
             </View>
             <View style={styles.emailsContainer}>
-              <Text>Emails</Text>
+              <Image source={email} resizeMode="contain" style={styles.image} />
             </View>
             <View style={styles.socialmediaContainer}>
-              <Text>Social Media</Text>
+              <Image
+                source={socialmedia}
+                resizeMode="contain"
+                style={styles.image}
+              />
             </View>
             <View style={styles.ewalletContainer}>
-              <Text>Ewallets</Text>
+              <Image
+                source={ewallet}
+                resizeMode="contain"
+                style={styles.image}
+              />
             </View>
           </View>
+
+          <View style={styles.logoutContainer}>
+            <Image source={logout} resizeMode="contain" style={styles.image} />
+          </View>
         </View>
-      </View>
+      </ImageBackground>
       <View style={styles.passwordsContainer}>
         <View style={styles.headerContainer}>
           <View style={styles.headerWrapper}>
             <View style={styles.logoContainer}>
-              <Text>Logo</Text>
+              <Image source={logo} resizeMode="contain" style={styles.logo} />
             </View>
             <View style={styles.infoContainer}>
               <View style={styles.allContainer}>
@@ -64,12 +99,15 @@ export default Crypt;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: "#e6e6e6",
     flexDirection: "row",
     alignItems: "flex-start",
     justifyContent: "center",
     height: screenHeight - StatusBar.currentHeight * 2,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop:
+      Platform.OS === "android"
+        ? StatusBar.currentHeight
+        : StatusBar.currentHeight * 2,
     borderWidth: 1,
     borderColor: "red",
   },
@@ -82,36 +120,99 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     borderWidth: 1,
-    borderColor: "blue",
+    borderColor: "yellow",
+    justifyContent: "space-between",
 
     // height: screenHeight - StatusBar.currentHeight * 3,
-    height: "93%",
+    height: "90%",
   },
   passwordsContainer: {
     borderWidth: 1,
     borderColor: "red",
     flex: 1,
-    height: "93%",
+    height: "90%",
   },
   headerContainer: {
-    borderWidth: 1,
-    borderColor: "red",
+    // borderWidth: 1,
+    // borderColor: "red",
     flexDirection: "row",
-    height: StatusBar.currentHeight * 2,
+    height: StatusBar.currentHeight * 2.5,
   },
   headerWrapper: {
-    borderWidth: 1,
-    borderColor: "red",
+    // borderWidth: 1,
+    // borderColor: "red",
     flexDirection: "row",
     justifyContent: "space-between",
     width: "80%",
   },
   infoContainer: {
-    borderWidth: 1,
-    borderColor: "red",
+    // borderWidth: 1,
+    // borderColor: "red",
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
     width: "80%",
+  },
+  backContainer: {
+    // borderWidth: 1,
+    // borderColor: "red",
+    height: StatusBar.currentHeight * 2.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "80%",
+  },
+  logo: {
+    width: "70%",
+  },
+
+  accountTypeContainer: {
+    // borderWidth: 1,
+    // borderColor: "blue",
+    height: screenHeight / 2.3,
+    justifyContent: "space-evenly",
+  },
+  logoutContainer: {
+    borderWidth: 1,
+    borderColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+    height: screenHeight / 6,
+  },
+  logoContainer: {
+    // borderWidth: 1,
+    // borderColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "20%",
+  },
+  emailsContainer: {
+    // borderWidth: 1,
+    // borderColor: "red",
+    height: StatusBar.currentHeight * 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ewalletContainer: {
+    // borderWidth: 1,
+    // borderColor: "red",
+    height: StatusBar.currentHeight * 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  socialmediaContainer: {
+    // borderWidth: 1,
+    // borderColor: "red",
+    height: StatusBar.currentHeight * 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  favoritesContainer: {
+    // borderWidth: 1,
+    // borderColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+    height: StatusBar.currentHeight * 3,
   },
 });
